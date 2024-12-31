e9296058642018d6c2d23ce251d86db11d48b84f
hoangnguyen <wandaandita2003@gmail.com>
2024-12-31 20:06:02 +0700

    fix: add url base on session

--- components/get-started.tsx
+++ components/get-started.tsx
@@ -1,14 +1,18 @@
 import Link from "next/link";
+import { getSession } from "next-auth/react";
 
 import { buttonVariants } from "@/components/ui/button";
 import { ArrowChatGPT } from "@/components/icon/icon";
 import { checkEnvironment } from "@/lib/utils";
 
 export default async function GetStartedButton() {
+  const session = await getSession(handler);
+  const url = session ? "/chat" : "/login";
+
   return (
     <Link
       className={buttonVariants({ size: "lg" })}
-      href={checkEnvironment().concat("/chat")}
+      href={checkEnvironment().concat(url)}
     >
       Get started <ArrowChatGPT props="ml-2" />
     </Link>
