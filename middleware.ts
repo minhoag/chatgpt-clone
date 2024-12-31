31baf111ebfd6a8d9e9475e19180d70de3b9e8d6
hoangnguyen <wandaandita2003@gmail.com>
2024-12-31 18:22:23 +0700

    change: AUTH to NEXTAUTH_SECRET for production

--- middleware.ts
+++ middleware.ts
@@ -5,9 +5,14 @@ import { NextResponse } from "next/server";
 export async function middleware(request: NextRequest) {
   const token = request.cookies.get("next-auth.session-token");
   const isAuthPage = request.nextUrl.pathname.startsWith("/auth");
+  const isChatPage = request.nextUrl.pathname.startsWith("/chat");
 
-  if (!token && !isAuthPage) {
-    return NextResponse.redirect(new URL("/login", request.url));
+  if (!token && !isAuthPage && isChatPage) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  if (!token && isChatPage) {
+    return NextResponse.redirect(new URL("/", request.url));
   }
 
   if (token && isAuthPage) {
