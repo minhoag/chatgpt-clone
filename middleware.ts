9015124f342f85ef16e7d72f16bce624525dae6b
hoangnguyen <wandaandita2003@gmail.com>
2024-12-31 19:57:35 +0700

    fix: fix middleware token

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,13 @@
 import type { NextRequest } from "next/server";
 
+import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  const token = request.cookies.get("next-auth.session-token");
+  const token = await getToken({
+    req: request,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
   const isAuthPage: boolean = request.nextUrl.pathname.startsWith("/auth");
   const isChatPage: boolean = request.nextUrl.pathname.startsWith("/chat");
 
