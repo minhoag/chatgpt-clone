321e126c710fbc327612166a46da9f45788f63ae
hoangnguyen <wandaandita2003@gmail.com>
2025-01-01 14:00:42 +0700

    fix: vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    JWT_SECRET: process.env.JWT_SECRET,
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     VERCEL_URL: process.env.VERCEL_URL,
-    DATABASE_URL: process.env.DATABASE_URL,
+    NODE_ENV: "production",
   },
 };
 
