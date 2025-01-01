ccc73a56270aa606cc03bd611cee4e2024d8164c
hoangnguyen <wandaandita2003@gmail.com>
2025-01-01 11:05:53 +0700

    fix: adjust size of input on mobile to prevent unexpected zoom out

--- components/chat-bubble.tsx
+++ components/chat-bubble.tsx
@@ -15,7 +15,7 @@ export function ChatBubble({ id, question, answer }: ChatBubbleProps) {
     <>
       <article
         key={id + "Q"}
-        className="flex w-max max-w-[75%] markdown prose break-words dark:prose-invert rounded-lg px-3 py-2 text-xs ml-auto bg-primary text-primary-foreground"
+        className="flex w-max max-w-[75%] markdown prose break-words dark:prose-invert rounded-lg px-3 py-2 text-xs ml-auto bg-primary text-primary-foreground md:text-base"
       >
         {question}
       </article>
