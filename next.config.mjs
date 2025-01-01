fc3bed4e1b8dfaba3c826c5773d0f5e7d58fea69
hoangnguyen <wandaandita2003@gmail.com>
2025-01-01 13:42:25 +0700

    feat: add loading to login and register for better UX

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    JWT_SECRET: process.env.JWT_SECRET,
-    AUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    AUTH_URL: process.env.NEXTAUTH_URL,
-    DATABASE_URL: process.env.DATABASE_URL,
+    VERCEL_URL: process.env.VERCEL_URL,
   },
 };
 
