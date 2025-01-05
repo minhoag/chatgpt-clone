d99c57568328c19545a496f9adcfab2c632e7e61
hoangnguyen <wandaandita2003@gmail.com>
2025-01-05 09:43:28 +0700

    fix: logout direct to localhost

--- components/logout.tsx
+++ components/logout.tsx
@@ -2,6 +2,7 @@ import { signOut } from "next-auth/react";
 
 import { Button } from "@/components/ui/button";
 import { LogOutIcon } from "@/components/icon/icon";
+import { checkEnvironment } from "@/lib/utils";
 
 export default function Logout() {
   return (
@@ -9,7 +10,7 @@ export default function Logout() {
       <Button
         className="text-base"
         variant="link"
-        onClick={() => signOut({ callbackUrl: "/" })}
+        onClick={() => signOut({ callbackUrl: checkEnvironment() })}
       >
         <span className="hidden text-sm lg:block">Logout </span>
         <LogOutIcon className="h-3 w-3" />
