8396f44cc4ba1a1c0c6fce0522cdcbb7c3646824
root <wandaandita2003@gmail.com>
2025-01-21 01:21:22 +0700

    remove check environment for self-deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["localhost:3000", "127.0.0.1:3000", "35.247.173.213"],
+    },
+  },
   env: {
     DATABASE_URL: process.env.DATABASE_URL,
     JWT_SECRET: process.env.JWT_SECRET,
